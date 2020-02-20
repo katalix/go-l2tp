dd34ebcc118817225d3ab7c0513b40581a0191be
Tom Parkin <tparkin@katalix.com>
2020-02-20 18:27:49 +0000

    l2tp: add control message parsing and interfaces (partial)

---
+++ l2tp/msg.go
@@ -0,0 +1,223 @@
+package l2tp
+
+import (
+	"bytes"
+	"encoding/binary"
+	"errors"
+	"fmt"
+	"io"
+
+	"github.com/katalix/sl2tpd/internal/nll2tp"
+)
+
+// L2TPv2 and L2TPv3 headers have these fields in common
+type l2tpCommonHeader struct {
+	FlagsVer uint16
+	Len      uint16
+}
+
+// L2TPv2 control message header per RFC2661
+type l2tpV2Header struct {
+	Common l2tpCommonHeader
+	Tid    uint16
+	Sid    uint16
+	Ns     uint16
+	Nr     uint16
+}
+
+// L2TPv3 control message header per RFC3931
+type l2tpV3Header struct {
+	Common l2tpCommonHeader
+	Ccid   uint32
+	Ns     uint16
+	Nr     uint16
+}
+
+const (
+	controlMessageMinLen = 12
+	controlMessageMaxLen = ^uint16(0)
+	commonHeaderLen      = 4
+	v2HeaderLen          = 12
+	v3HeaderLen          = 12
+)
+
+func (h *l2tpCommonHeader) protocolVersion() (version nll2tp.L2tpProtocolVersion, err error) {
+	switch h.FlagsVer & 0xf {
+	case 2:
+		return nll2tp.ProtocolVersion2, nil
+	case 3:
+		return nll2tp.ProtocolVersion3, nil
+	}
+	return nll2tp.ProtocolVersionInvalid, errors.New("illegal protocol version")
+}
+
+func newL2tpV2ControlMessage(b []byte) (msg *L2tpV2ControlMessage, err error) {
+	var hdr l2tpV2Header
+	var avps []AVP
+
+	r := bytes.NewReader(b)
+	if err = binary.Read(r, binary.BigEndian, &hdr); err != nil {
+		return nil, err
+	}
+	if avps, err = ParseAVPBuffer(b[v2HeaderLen:hdr.Common.Len]); err != nil {
+		return nil, err
+	}
+	return &L2tpV2ControlMessage{
+		header: hdr,
+		avps:   avps,
+	}, nil
+}
+
+func newL2tpV3ControlMessage(b []byte) (msg *L2tpV3ControlMessage, err error) {
+	return nil, errors.New("newL2tpV3ControlMessage not implemented")
+}
+
+// L2tpControlMessage is an interface representing a generic L2TP
+// control message, providing access to the fields that are common
+// to both v2 and v3 versions of the protocol.
+type L2tpControlMessage interface {
+	ProtocolVersion() nll2tp.L2tpProtocolVersion
+	Len() int
+	Ns() uint16
+	Nr() uint16
+	Avps() []AVP
+}
+
+// L2tpV2ControlMessage represents an RFC2661 control message
+type L2tpV2ControlMessage struct {
+	header l2tpV2Header
+	avps   []AVP
+}
+
+// L2tpV3ControlMessage represents an RFC3931 control message
+type L2tpV3ControlMessage struct {
+	header l2tpV3Header
+	avps   []AVP
+}
+
+// ProtocolVersion returns the protocol version for the control message.
+// Implements the L2tpControlMessage interface.
+func (m *L2tpV2ControlMessage) ProtocolVersion() nll2tp.L2tpProtocolVersion {
+	return nll2tp.ProtocolVersion2
+}
+
+// Len returns the total control message length, including the header, in octets.
+// Implements the L2tpControlMessage interface.
+func (m *L2tpV2ControlMessage) Len() int {
+	return int(m.header.Common.Len)
+}
+
+// Ns returns the L2TP transport Ns value for the message.
+// Implements the L2tpControlMessage interface.
+func (m *L2tpV2ControlMessage) Ns() uint16 {
+	return m.header.Ns
+}
+
+// Nr returns the L2TP transport Ns value for the message.
+// Implements the L2tpControlMessage interface.
+func (m *L2tpV2ControlMessage) Nr() uint16 {
+	return m.header.Nr
+}
+
+// Avps returns the slice of Attribute Value Pair (AVP) values held by the control message.
+// Implements the L2tpControlMessage interface.
+func (m *L2tpV2ControlMessage) Avps() []AVP {
+	return m.avps
+}
+
+// Tid returns the L2TPv2 tunnel ID held by the control message header.
+func (m *L2tpV2ControlMessage) Tid() uint16 {
+	return m.header.Tid
+}
+
+// Sid returns the L2TPv2 session ID held by the control message header.
+func (m *L2tpV2ControlMessage) Sid() uint16 {
+	return m.header.Sid
+}
+
+// ProtocolVersion returns the protocol version for the control message.
+// Implements the L2tpControlMessage interface.
+func (m *L2tpV3ControlMessage) ProtocolVersion() nll2tp.L2tpProtocolVersion {
+	return nll2tp.ProtocolVersion3
+}
+
+// Len returns the total control message length, including the header, in octets.
+// Implements the L2tpControlMessage interface.
+func (m *L2tpV3ControlMessage) Len() int {
+	return int(m.header.Common.Len)
+}
+
+// Ns returns the L2TP transport Ns value for the message.
+// Implements the L2tpControlMessage interface.
+func (m *L2tpV3ControlMessage) Ns() uint16 {
+	return m.header.Ns
+}
+
+// Nr returns the L2TP transport Ns value for the message.
+// Implements the L2tpControlMessage interface.
+func (m *L2tpV3ControlMessage) Nr() uint16 {
+	return m.header.Nr
+}
+
+// Avps returns the slice of Attribute Value Pair (AVP) values held by the control message.
+// Implements the L2tpControlMessage interface.
+func (m *L2tpV3ControlMessage) Avps() []AVP {
+	return m.avps
+}
+
+// ControlConnectionID returns the control connection ID held by the control message header.
+func (m *L2tpV3ControlMessage) ControlConnectionID() uint32 {
+	return m.header.Ccid
+}
+
+// ParseMessageBuffer takes a byte slice of L2TP control message data and
+// parses it into an array of L2tpControlMessage instances.
+func ParseMessageBuffer(b []byte) (messages []L2tpControlMessage, err error) {
+	r := bytes.NewReader(b)
+	for r.Len() >= controlMessageMinLen {
+		var h l2tpCommonHeader
+		var cursor int64
+
+		if cursor, err = r.Seek(0, io.SeekCurrent); err != nil {
+			return nil, errors.New("malformed message buffer: unable to determine current offset")
+		}
+
+		// Read the common part of the header: this will tell us the
+		// protocol version and the length of the complete frame
+		if err := binary.Read(r, binary.BigEndian, &h); err != nil {
+			return nil, err
+		}
+
+		// Throw out malformed packets
+		if int(h.Len-commonHeaderLen) > r.Len() {
+			return nil, fmt.Errorf("malformed header: length %d exceeds buffer bounds of %d", h.Len, r.Len())
+		}
+
+		// Figure out the protocol version, and read the message
+		if ver, err := h.protocolVersion(); err != nil {
+			return nil, err
+		} else {
+			if ver == nll2tp.ProtocolVersion2 {
+				if msg, err := newL2tpV2ControlMessage(b[cursor : cursor+int64(h.Len)]); err != nil {
+					return nil, err
+				} else {
+					messages = append(messages, msg)
+				}
+			} else if ver == nll2tp.ProtocolVersion3 {
+				if msg, err := newL2tpV3ControlMessage(b[cursor : cursor+int64(+h.Len)]); err != nil {
+					return nil, err
+				} else {
+					messages = append(messages, msg)
+				}
+			} else {
+				panic("Unhandled protocol version")
+			}
+		}
+
+		// Step on to the next message in the buffer, if any
+		if _, err := r.Seek(int64(h.Len), io.SeekCurrent); err != nil {
+			return nil, errors.New("malformed message buffer: invalid length for current message")
+		}
+	}
+	return messages, nil
+}
---
+++ l2tp/msg_test.go
@@ -0,0 +1,83 @@
+package l2tp
+
+import (
+	//"fmt"
+	//	"reflect"
+	"testing"
+
+	"github.com/katalix/sl2tpd/internal/nll2tp"
+)
+
+type msgInfo struct {
+	version          nll2tp.L2tpProtocolVersion
+	ns, nr, tid, sid uint16
+	ccid             uint32
+	navps            int
+}
+
+func TestParseMessageBuffer(t *testing.T) {
+	cases := []struct {
+		in   []byte
+		want []msgInfo
+	}{
+		{
+			in: []byte{
+				0xc8, 0x02, 0x00, 0x14, 0x00, 0x01, 0x00, 0x00,
+				0x00, 0x01, 0x00, 0x01, 0x80, 0x08, 0x00, 0x00,
+				0x00, 0x00, 0x00, 0x06,
+			},
+			want: []msgInfo{
+				{version: nll2tp.ProtocolVersion2, ns: 1, nr: 1, tid: 1, sid: 0, navps: 1},
+			},
+		},
+	}
+	for _, c := range cases {
+		got, err := ParseMessageBuffer(c.in)
+		if err == nil {
+			for i, g := range got {
+				// common checks
+				if g.ProtocolVersion() != c.want[i].version {
+					t.Errorf("ProtocolVersion() == %q, want %q", g.ProtocolVersion(), c.want[i].version)
+				}
+				if g.Len() != len(c.in) {
+					t.Errorf("Len() == %q, want %q", g.Len(), len(c.in))
+				}
+				if g.Ns() != c.want[i].ns {
+					t.Errorf("Ns() == %q, want %q", g.Ns(), c.want[i].ns)
+				}
+				if g.Nr() != c.want[i].nr {
+					t.Errorf("Nr() == %q, want %q", g.Nr(), c.want[i].nr)
+				}
+				if len(g.Avps()) != c.want[i].navps {
+					t.Errorf("AVP count failed: got %q, want %q", len(g.Avps()), c.want[i].navps)
+				}
+				// version specifics
+				switch c.want[i].version {
+				case nll2tp.ProtocolVersion2:
+					v2msg, ok := g.(*L2tpV2ControlMessage)
+					if ok {
+						if v2msg.Tid() != c.want[i].tid {
+							t.Errorf("Tid() == %q, want %q", v2msg.Tid(), c.want[i].tid)
+						}
+						if v2msg.Sid() != c.want[i].sid {
+							t.Errorf("Sid() == %q, want %q", v2msg.Sid(), c.want[i].sid)
+						}
+					} else {
+						t.Errorf("Expected L2tpV2ControlMessage, but didn't receive one")
+					}
+				case nll2tp.ProtocolVersion3:
+					v3msg, ok := g.(*L2tpV3ControlMessage)
+					if ok {
+						if v3msg.ControlConnectionID() != c.want[i].ccid {
+							t.Errorf("ControlConnectionID() == %q, want %q", v3msg.ControlConnectionID(), c.want[i].ccid)
+						}
+					} else {
+						t.Errorf("Expected L2tpV3ControlMessage, but didn't receive one")
+					}
+				}
+			}
+		} else {
+			t.Errorf("ParseMessageBuffer(%q) failed: %q", c.in, err)
+		}
+	}
+}
