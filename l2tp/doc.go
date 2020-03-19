a0256f8fa2fa497c08d12fdc995f43ccfb8ad7f5
Tom Parkin <tparkin@katalix.com>
2020-03-19 11:14:07 +0000

    l2tp: add minimal doc.go file for package level documentation

---
+++ l2tp/doc.go
@@ -0,0 +1,8 @@
+// Package l2tp implements the Layer 2 Tunneling Protocol as per
+// RFC2661 (L2TPv2) and RFC3931 (L2TPv3).
+//
+// Limitations
+//
+//  * Currently support for the control protocol is very limited.
+//  * Only Linux systems are supported for the data plane.
+package l2tp
