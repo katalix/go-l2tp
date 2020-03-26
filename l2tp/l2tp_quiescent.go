b46a61c12c7d334402a5ade0dc68ed0ee221c8d1
Tom Parkin <tparkin@katalix.com>
2020-03-26 21:28:33 +0000

    l2tp: split static and quiescent instance code into seperate files

---
+++ l2tp/l2tp_quiescent.go
@@ -0,0 +1,171 @@
+package l2tp
+
+import (
+	"fmt"
+	"sync"
+	"time"
+
+	"github.com/go-kit/kit/log"
+	"github.com/go-kit/kit/log/level"
+	"github.com/katalix/l2tp/internal/nll2tp"
+	"golang.org/x/sys/unix"
+)
+
+type quiescentTunnel struct {
+	logger    log.Logger
+	name      string
+	parent    *Context
+	cfg       *TunnelConfig
+	cp        *l2tpControlPlane
+	xport     *transport
+	dp        dataPlane
+	closeChan chan bool
+	wg        sync.WaitGroup
+	sessions  map[string]Session
+}
+
+func (qt *quiescentTunnel) NewSession(name string, cfg *SessionConfig) (Session, error) {
+
+	if _, ok := qt.sessions[name]; ok {
+		return nil, fmt.Errorf("already have session %q", name)
+	}
+
+	s, err := newStaticSession(name, qt, cfg)
+	if err != nil {
+		return nil, err
+	}
+
+	qt.sessions[name] = s
+
+	return s, nil
+}
+
+func (qt *quiescentTunnel) Close() {
+	if qt != nil {
+		qt.closeChan <- true
+		qt.wg.Wait()
+		qt.close()
+	}
+}
+
+func (qt *quiescentTunnel) close() {
+	if qt != nil {
+		for name, session := range qt.sessions {
+			session.Close()
+			qt.unlinkSession(name)
+		}
+
+		if qt.xport != nil {
+			qt.xport.close()
+		}
+		if qt.cp != nil {
+			qt.cp.close()
+		}
+		if qt.dp != nil {
+			qt.dp.close(qt.getNLConn())
+		}
+
+		qt.parent.unlinkTunnel(qt.name)
+
+		level.Info(qt.logger).Log("message", "close")
+	}
+}
+
+func (qt *quiescentTunnel) getCfg() *TunnelConfig {
+	return qt.cfg
+}
+
+func (qt *quiescentTunnel) getNLConn() *nll2tp.Conn {
+	return qt.parent.nlconn
+}
+
+func (qt *quiescentTunnel) getLogger() log.Logger {
+	return qt.logger
+}
+
+func (qt *quiescentTunnel) unlinkSession(name string) {
+	delete(qt.sessions, name)
+}
+
+func (qt *quiescentTunnel) xportReader() {
+	// Although we're not running the control protocol we do need
+	// to drain messages from the transport to avoid the receive
+	// path blocking.
+	defer qt.wg.Done()
+	for {
+		select {
+		case <-qt.closeChan:
+			return
+		case _, ok := <-qt.xport.recvChan:
+			if !ok {
+				qt.close()
+				return
+			}
+		}
+	}
+}
+
+func newQuiescentTunnel(name string, parent *Context, sal, sap unix.Sockaddr, cfg *TunnelConfig) (qt *quiescentTunnel, err error) {
+	qt = &quiescentTunnel{
+		logger:    log.With(parent.logger, "tunnel_name", name),
+		name:      name,
+		parent:    parent,
+		cfg:       cfg,
+		closeChan: make(chan bool),
+		sessions:  make(map[string]Session),
+	}
+
+	// Initialise the control plane.
+	// We bind/connect immediately since we're not runnning most of the control protocol.
+	qt.cp, err = newL2tpControlPlane(sal, sap)
+	if err != nil {
+		qt.Close()
+		return nil, err
+	}
+
+	err = qt.cp.bind()
+	if err != nil {
+		qt.Close()
+		return nil, err
+	}
+
+	err = qt.cp.connect()
+	if err != nil {
+		qt.Close()
+		return nil, err
+	}
+
+	qt.dp, err = newManagedTunnelDataPlane(parent.nlconn, qt.cp.fd, cfg)
+	if err != nil {
+		qt.Close()
+		return nil, err
+	}
+
+	qt.xport, err = newTransport(qt.logger, qt.cp, transportConfig{
+		HelloTimeout:      cfg.HelloTimeout,
+		TxWindowSize:      cfg.WindowSize,
+		MaxRetries:        cfg.MaxRetries,
+		RetryTimeout:      cfg.RetryTimeout,
+		AckTimeout:        time.Millisecond * 100,
+		Version:           cfg.Version,
+		PeerControlConnID: cfg.PeerTunnelID,
+	})
+	if err != nil {
+		qt.Close()
+		return nil, err
+	}
+
+	qt.wg.Add(1)
+	go qt.xportReader()
+
+	level.Info(qt.logger).Log(
+		"message", "new quiescent tunnel",
+		"version", cfg.Version,
+		"encap", cfg.Encap,
+		"local", cfg.Local,
+		"peer", cfg.Peer,
+		"tunnel_id", cfg.TunnelID,
+		"peer_tunnel_id", cfg.PeerTunnelID)
+
+	return
+}
---
+++ l2tp/l2tp_static.go
@@ -0,0 +1,136 @@
+package l2tp
+
+import (
+	"fmt"
+
+	"github.com/go-kit/kit/log"
+	"github.com/go-kit/kit/log/level"
+	"github.com/katalix/l2tp/internal/nll2tp"
+	"golang.org/x/sys/unix"
+)
+
+type staticTunnel struct {
+	logger   log.Logger
+	name     string
+	parent   *Context
+	cfg      *TunnelConfig
+	dp       dataPlane
+	sessions map[string]Session
+}
+
+type staticSession struct {
+	logger log.Logger
+	name   string
+	parent Tunnel
+	cfg    *SessionConfig
+	dp     dataPlane
+}
+
+func (st *staticTunnel) NewSession(name string, cfg *SessionConfig) (Session, error) {
+
+	if _, ok := st.sessions[name]; ok {
+		return nil, fmt.Errorf("already have session %q", name)
+	}
+
+	s, err := newStaticSession(name, st, cfg)
+
+	if err != nil {
+		return nil, err
+	}
+
+	st.sessions[name] = s
+
+	return s, nil
+}
+
+func (st *staticTunnel) Close() {
+	if st != nil {
+
+		for name, session := range st.sessions {
+			session.Close()
+			st.unlinkSession(name)
+		}
+
+		if st.dp != nil {
+			st.dp.close(st.getNLConn())
+		}
+
+		st.parent.unlinkTunnel(st.name)
+
+		level.Info(st.logger).Log("message", "close")
+	}
+}
+
+func (st *staticTunnel) getCfg() *TunnelConfig {
+	return st.cfg
+}
+
+func (st *staticTunnel) getNLConn() *nll2tp.Conn {
+	return st.parent.nlconn
+}
+
+func (st *staticTunnel) getLogger() log.Logger {
+	return st.logger
+}
+
+func (st *staticTunnel) unlinkSession(name string) {
+	delete(st.sessions, name)
+}
+
+func newStaticTunnel(name string, parent *Context, sal, sap unix.Sockaddr, cfg *TunnelConfig) (st *staticTunnel, err error) {
+	st = &staticTunnel{
+		logger:   log.With(parent.logger, "tunnel_name", name),
+		name:     name,
+		parent:   parent,
+		cfg:      cfg,
+		sessions: make(map[string]Session),
+	}
+
+	st.dp, err = newStaticTunnelDataPlane(parent.nlconn, sal, sap, cfg)
+	if err != nil {
+		st.Close()
+		return nil, err
+	}
+
+	level.Info(st.logger).Log(
+		"message", "new static tunnel",
+		"version", cfg.Version,
+		"encap", cfg.Encap,
+		"local", cfg.Local,
+		"peer", cfg.Peer,
+		"tunnel_id", cfg.TunnelID,
+		"peer_tunnel_id", cfg.PeerTunnelID)
+
+	return
+}
+
+func newStaticSession(name string, parent Tunnel, cfg *SessionConfig) (ss *staticSession, err error) {
+	// Since we're static we instantiate the session in the
+	// dataplane at the point of creation.
+	dp, err := newSessionDataPlane(parent.getNLConn(), parent.getCfg().TunnelID, parent.getCfg().PeerTunnelID, cfg)
+	if err != nil {
+		return
+	}
+
+	ss = &staticSession{
+		logger: log.With(parent.getLogger(), "session_name", name),
+		name:   name,
+		parent: parent,
+		cfg:    cfg,
+		dp:     dp,
+	}
+
+	level.Info(ss.logger).Log(
+		"message", "new static session",
+		"session_id", cfg.SessionID,
+		"peer_session_id", cfg.PeerSessionID,
+		"pseudowire", cfg.Pseudowire)
+
+	return
+}
+
+func (ss *staticSession) Close() {
+	ss.dp.close(ss.parent.getNLConn())
+	ss.parent.unlinkSession(ss.name)
+	level.Info(ss.logger).Log("message", "close")
+}
