a1ce1bbc78846d134100aa358746863e80a979dc
Tom Parkin <tparkin@katalix.com>
2020-05-14 17:20:08 +0100

    kl2tpd: add pppd exit code stringifier

--- cmd/kl2tpd/pppol2tp.go
+++ cmd/kl2tpd/pppol2tp.go
@@ -137,3 +137,50 @@ func newPPPoL2TP(tunnelID, sessionID, peerTunnelID, peerSessionID l2tp.ControlCo
 		stderrBuf: &stderr,
 	}, nil
 }
+
+func pppdExitCodeString(err error) string {
+	// ref: pppd(8) section EXIT STATUS
+	switch err.Error() {
+	case "exit status 0":
+		return "pppd established successfully and terminated at peer's request"
+	case "exit status 1":
+		return "immediately fatal error (e.g. essential system call failed, or out of memory)"
+	case "exit status 2":
+		return "error detected during options parsing/processing"
+	case "exit status 3":
+		return "pppd is not setuid-root and the invoking user is not root"
+	case "exit status 4":
+		return "the kernel does not support PPP (possibly the module is not loaded or unavailable)"
+	case "exit status 5":
+		return "pppd terminated due to SIGINT, SIGTERM, or SIGHUP signal"
+	case "exit status 6":
+		return "the serial port could not be locked"
+	case "exit status 7":
+		return "the serial port could not be opened"
+	case "exit status 8":
+		return "the connect script returned a non-zero exit status"
+	case "exit status 9":
+		return "the command specified as an argument to the pty option could not be run"
+	case "exit status 10":
+		return "PPP negotiation failed (that is, didn't reach the point where at least one network protocol was running)"
+	case "exit status 11":
+		return "the peer system failed or refused to authenticate itself"
+	case "exit status 12":
+		return "the link was established successfully and terminated because it was idle"
+	case "exit status 13":
+		return "the link was established successfully and terminated because the connect time limit was reached"
+	case "exit status 14":
+		return "callback was negotiated and an incoming call should arrive shortly"
+	case "exit status 15":
+		return "the link was terminated because the peer is not responding to echo requests"
+	case "exit status 16":
+		return "the link was terminated by the modem hanging up"
+	case "exit status 17":
+		return "the ppp negotiation failed because serial loopback was detected"
+	case "exit status 18":
+		return "the init script returned a non-zero exit status"
+	case "exit status 19":
+		return "we failed to authenticate ourselves to the peer"
+	}
+	return err.Error()
+}
