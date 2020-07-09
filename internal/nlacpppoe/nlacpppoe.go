f658b3e554cdb10e33fa699fe3617560370cdc15
Tom Parkin <tparkin@katalix.com>
2020-07-09 13:22:13 +0100

    nlacpppoe: check args for route add/del, and add missing nl attribute to message

--- internal/nlacpppoe/nlacpppoe.go
+++ internal/nlacpppoe/nlacpppoe.go
@@ -39,10 +39,21 @@ func (conn *Conn) addDelRoute(cmd uint8,
 	l2tpTunnelId, l2tpSessionId, l2tpPeerSessionId uint32,
 	pppoeSessionId uint16, ifname string) (err error) {
 
+	if l2tpTunnelId == 0 {
+		return fmt.Errorf("L2TP tunnel ID must be nonzero")
+	} else if l2tpSessionId == 0 {
+		return fmt.Errorf("L2TP session ID must be nonzero")
+	} else if pppoeSessionId == 0 {
+		return fmt.Errorf("PPPoE session ID must be nonzero")
+	} else if ifname == "" {
+		return fmt.Errorf("PPPoE interface name must be specified")
+	}
+
 	ae := netlink.NewAttributeEncoder()
 	ae.Uint32(AttrL2TPTunnelId, l2tpTunnelId)
 	ae.Uint32(AttrL2TPSessionId, l2tpSessionId)
 	ae.Uint32(AttrL2TPPeerSessionId, l2tpPeerSessionId)
+	ae.Uint16(AttrPPPoESessionId, pppoeSessionId)
 	ae.String(AttrPPPoEIfname, ifname)
 
 	b, err := ae.Encode()
