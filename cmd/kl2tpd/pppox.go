6e56be4ce6340d162686d07a3453f857d32b84e0
Tom Parkin <tparkin@katalix.com>
2020-11-05 09:27:01 +0000

    kl2tpd: add helpers for pppoe sockaddr_pppox

--- cmd/kl2tpd/pppox.go
+++ cmd/kl2tpd/pppox.go
@@ -6,11 +6,72 @@ package main
 import "C"
 
 import (
+	"encoding/binary"
 	"fmt"
 	"unsafe"
 
 	"github.com/katalix/go-l2tp/l2tp"
 )
+
+/*
+struct sockaddr_pppox uses the gcc attribute "packed", so cgo isn't
+able to infer the fields of the structure.  We have to manually pack it.
+
+typedef __be16 sid_t;
+struct pppoe_addr {
+	sid_t         sid;
+	unsigned char remote[ETH_ALEN];
+	char          dev[IFNAMSIZ];
+};
+
+struct sockaddr_pppox {
+	__kernel_sa_family_t sa_family;
+	unsigned int    sa_protocol;
+	union {
+		struct pppoe_addr  pppoe;
+		struct pptp_addr   pptp;
+	} sa_addr;
+} __attribute__((packed));
+*/
+func newSockaddrPPPoE(sessionID uint16, destHWAddr [6]byte, interfaceName string) (
+	addr *C.struct_sockaddr,
+	addrLen C.socklen_t,
+	err error) {
+	if sessionID == 0 {
+		return nil, 0, fmt.Errorf("session ID must be greater than zero")
+	}
+	if interfaceName == "" {
+		return nil, 0, fmt.Errorf("interface name cannot be empty")
+	}
+	if len(interfaceName) > C.IFNAMSIZ-1 {
+		return nil, 0, fmt.Errorf("interface name length cannot be greater than IFNAMSIZ")
+	}
+
+	var sa C.struct_sockaddr_pppox
+	buf := (*[C.sizeof_struct_sockaddr_pppox]byte)(unsafe.Pointer(&sa))
+	idx := 0
+
+	// struct sockaddr_pppox -> sa_family
+	*(*C.ushort)(unsafe.Pointer(&buf[idx])) = C.AF_PPPOX
+	idx += C.sizeof_ushort
+
+	// struct sockaddr_pppox -> sa_protocol
+	*(*C.uint)(unsafe.Pointer(&buf[idx])) = C.PX_PROTO_OE
+	idx += C.sizeof_uint
+
+	// struct sockaddr_pppox -> sa_addr -> pppoe -> sid
+	binary.BigEndian.PutUint16(buf[idx:idx+C.sizeof___u16], sessionID)
+	idx += C.sizeof___u16
+
+	// struct sockaddr_pppox -> sa_addr -> pppoe -> remote
+	copy(buf[idx:idx+len(destHWAddr)], destHWAddr[:])
+	idx += len(destHWAddr)
+
+	// struct sockaddr_pppox -> sa_addr -> pppoe -> dev
+	copy(buf[idx:], interfaceName)
+
+	return (*C.struct_sockaddr)(unsafe.Pointer(&sa)), C.sizeof_struct_sockaddr_pppox, nil
+}
 
 /*
 struct sockaddr_pppol2tp uses the gcc attribute "packed", so cgo isn't
@@ -96,6 +157,25 @@ func socketPPPoL2TPv4(tunnelID, sessionID, peerTunnelID, peerSessionID l2tp.Cont
 	}
 
 	_fd, err := C.socket(C.AF_PPPOX, C.SOCK_DGRAM, C.PX_PROTO_OL2TP)
+	if _fd < 0 {
+		return -1, fmt.Errorf("failed to open pppox socket: %v", err)
+	}
+
+	ret, err := C.connect(_fd, addr, addrLen)
+	if ret < 0 {
+		C.close(_fd)
+		return -1, fmt.Errorf("failed to connect pppox socket: %v", err)
+	}
+	return int(fd), nil
+}
+
+func socketPPPoE(sessionID uint16, destHWAddr [6]byte, interfaceName string) (fd int, err error) {
+	addr, addrLen, err := newSockaddrPPPoE(sessionID, destHWAddr, interfaceName)
+	if err != nil {
+		return -1, fmt.Errorf("failed to build struct sockaddr_pppox: %v", err)
+	}
+
+	_fd, err := C.socket(C.AF_PPPOX, C.SOCK_DGRAM, C.PX_PROTO_OE)
 	if _fd < 0 {
 		return -1, fmt.Errorf("failed to open pppox socket: %v", err)
 	}
